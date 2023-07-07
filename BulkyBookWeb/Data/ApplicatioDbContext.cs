614fb8e7043cc7dfbbc1edd76723874f0295440e
studenticomdata <studenticomdata@outlook.com>
2023-07-08 00:01:35 +0200

    Add project files.

---
+++ BulkyBookWeb/Data/ApplicatioDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BulkyBookWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBookWeb.Data
+{
+    public class ApplicatioDbContext : DbContext
+    {
+        public ApplicatioDbContext(DbContextOptions<ApplicatioDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
